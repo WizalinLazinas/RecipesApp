d7c471b8221ecfc5e9b84ce5929d025639b39bf7
WizalinLazinas <kmelnikov55555@gmail.com>
2022-05-01 00:02:29 +0300

    Initial commit

---
+++ Ingredient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipesApp
+{
+    public record Ingredient
+    {
+        public string? Name { get; set; }
+        public double Amount { get; set; }
+        public string? Measurement { get; set; }
+        public double Price { get; set; }
+
+    }
+}
---
+++ Recipe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipesApp
+{
+    public record Recipe
+    {
+        public string? Name { get; set; }
+        public List<Ingredient>? Ingredients { get; set; }
+        public DateTime CookingTime { get; set; }
+        public double Price { get; set; }
+        public string? Description { get; set; }
+        public string? Instruction { get; set; }
+    }
+}
